c3e054ec23cdc5056dd45832972a6452bb7a00e8
RobynSpaulding <robynm27@gmail.com>
2023-01-23 13:30:09 -0800

    completed array reduce practice in ruby,TODO: complete problems in javascript

--- array_reduce.rb
+++ array_reduce.rb
@@ -63,14 +63,65 @@ string_length(["volleyball", "basketball", "badminton"])
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
+def lowest_price(array)
+  lowest_price = array[0]
+  array.each do |item|
+    if item[:price] < lowest_price[:price]
+      lowest_price = item
+    end
+  end
+  # p lowest_price
+end
+lowest_price([{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}])
+
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
+
+def product(array)
+  product = 1
+  array.each do |num|
+    product *= num
+  end
+  # p product
+end
+product([5, 10, 8, 3])
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
+def dashes(array)
+  new_string = "-"
+  array.each do |word|
+    new_string += + word + "-"
+  end
+  # p new_string
+end
+dashes(["volleyball", "basketball", "badminton"])
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 
+def shortest_name(array)
+  shortest_name = array[0]
+  array.each do |hash|
+    if hash[:name].length < shortest_name[:name].length
+      shortest_name = hash
+    end
+  end
+  # p shortest_name
+end
+shortest_name([{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}])
+
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
+
+def max_num(array)
+  max_num = array[0]
+  array.each do |num|
+    if num > max_num
+      max_num = num
+    end
+  end
+  # p max_num
+end
+
+max_num([5, 10, 8, 3])
