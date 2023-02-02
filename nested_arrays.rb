a1a0ca1ea00ad58cc5d80ff21d5b186acf20e8e7
RobynSpaulding <robynm27@gmail.com>
2023-02-02 11:11:04 -0800

    solves number 5 in nested_arrays.rb

--- nested_arrays.rb
+++ nested_arrays.rb
@@ -64,12 +64,23 @@ def max_product(num_array)
     end
     i += 1
   end
-  p max_product
+  # p max_product
 end
 max_product([5, -2, 1, -9, -7, 2, 6])
 
 #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
+
+def sum(arrays)
+  result = 0
+  arrays.each do |array|
+    array.each do |num|
+      result += num
+    end
+  end
+  p result
+end
+sum([[1, 3], [8, 9], [2, 16]])
 
 #  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
 #     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
