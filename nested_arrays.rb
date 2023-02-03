525098cc92270b150ef53e0002881bfbe7a06ab5
RobynSpaulding <robynm27@gmail.com>
2023-02-02 20:39:50 -0800

    soves number 7 in nested arrays

--- nested_arrays.rb
+++ nested_arrays.rb
@@ -99,6 +99,21 @@ array_sums([1, 2], [6, 7, 8])
 #  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 #     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
 
+def every_product(array)
+  products = []
+  index1 = 0
+  while index1 < array.length
+    index2 = 0
+    while index2 < array.length
+      products << array[index1]*array[index2]
+      index2+=1
+    end
+    index1+=1
+  end
+  p products
+end
+every_product([2, 8, 3])
+
 #  8. Use a nested loop to find the largest sum of any two different numbers within an array.
 #     For example, [1, 8, 3, 10] becomes 18.
 
