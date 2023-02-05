3e5913969e28d5866b8727ce73dd087cc469cd62
RobynSpaulding <robynm27@gmail.com>
2023-02-05 12:51:21 -0800

    adds conditionals_2.js file to solve in Javascript, solves numbers 1-5

---
+++ conditionals_2.js
@@ -0,0 +1,60 @@
+// # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+// var num = 10;
+// if (num === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
+
+// # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+// var num2 = 10;
+// if (num2 < 10) {
+//   console.log(-1);
+// } else if (num2 > 10) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+// # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+// var nums = [11, 2];
+// if (nums[0] < 10 && nums[1] < 10) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+// # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+// var num3 = 90;
+// if (num3 > 9000) {
+//   console.log(1);
+// } else {
+//   console.log(-1);
+// }
+
+// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+// var number = 15;
+// if (number < 10) {
+//   console.log(9);
+// } else if (number < 20) {
+//   console.log(19);
+// } else if (number < 30) {
+//   console.log(29);
+// } else {
+//   console.log(-1);
+// }
+
+// # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+// # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+// # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+// # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+// # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
