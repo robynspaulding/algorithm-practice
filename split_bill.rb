e6596e7d7dbe3cd38bafe531a72b872644a5b4cc
RobynSpaulding <robynm27@gmail.com>
2023-02-23 16:00:58 -0800

    adds and solves split_bill challenge

---
+++ split_bill.rb
@@ -0,0 +1,26 @@
+def bonAppetit(bill, k, b)
+  # Write your code here
+  # anna_total variable
+  # loop through bill 
+  # if currrent index not equal k 
+  # add to anna_total
+  # does anna_total/2 equal b
+  # if yes retirn "bonAppitit"
+  #else return difference between   b-anna_total
+  
+  anna_total = 0
+  index = 0
+  while index < bill.length
+      if index != k
+          anna_total += bill[index]
+      end
+      index += 1  
+  end
+  if anna_total/2 == b
+      p "Bon Appetit"
+  else
+      p anna_total/2 - b
+  end
+end
+
+bonAppetit([3, 10, 2, 9], 1, 7)
