1f90c7c343d8b49176298652ee76456beeff45de
RobynSpaulding <robynm27@gmail.com>
2023-04-05 18:47:57 -0700

    solves upcase question

--- methods_practice.rb
+++ methods_practice.rb
@@ -1,4 +1,7 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+puts "Type a word:"
+word = gets.chomp
+p word.upcase 
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
