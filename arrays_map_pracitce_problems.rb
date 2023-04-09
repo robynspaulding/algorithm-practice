f5ea3a1061c9634c2d902c56601c6f35f0d7875a
RobynSpaulding <robynm27@gmail.com>
2023-04-08 19:39:04 -0700

    solves number 8

--- arrays_map_pracitce_problems.rb
+++ arrays_map_pracitce_problems.rb
@@ -73,6 +73,13 @@ p result
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+words = ["hi", "sweet", "good"]
+first_letters = []
+words.each do |word|
+  first_letters << word[0]
+end
+p first_letters
+
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
