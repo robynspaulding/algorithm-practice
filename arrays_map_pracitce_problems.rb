172b8cfd4f26f4fed14c3ab0a0e001fbbf7d97fd
RobynSpaulding <robynm27@gmail.com>
2023-04-08 19:31:59 -0700

    solves number 6

--- arrays_map_pracitce_problems.rb
+++ arrays_map_pracitce_problems.rb
@@ -53,6 +53,13 @@ p sal_length
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+hashes2 = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+ages= []
+hashes2.each do |hash|
+  ages.push(hash[:age])
+end
+p ages
+
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
