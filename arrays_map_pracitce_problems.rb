9e9e9111fcf83b390fe52a6f3da6f7fb0fe48d87
RobynSpaulding <robynm27@gmail.com>
2023-04-08 19:40:09 -0700

    solves number 9

--- arrays_map_pracitce_problems.rb
+++ arrays_map_pracitce_problems.rb
@@ -83,5 +83,12 @@ p first_letters
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+hashes3 = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+ages_times2= []
+hashes2.each do |hash|
+  ages_times2 << hash[:age]*2
+end
+p ages_times2
+
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
