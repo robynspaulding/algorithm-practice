cd6d028606e8b07b66f32feefe2257381497f9ac
RobynSpaulding <robynm27@gmail.com>
2023-04-08 19:41:08 -0700

    solves number 10

--- arrays_map_pracitce_problems.rb
+++ arrays_map_pracitce_problems.rb
@@ -92,3 +92,10 @@ p ages_times2
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+numbers = [1, 2, 3]
+nums_to_strings = []
+numbers.each do |num|
+  nums_to_strings << num.to_s
+end
+p nums_to_strings
