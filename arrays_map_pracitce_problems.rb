71a6a8dd8d73e0d4574b89af5abacd0907805886
RobynSpaulding <robynm27@gmail.com>
2023-04-08 19:28:15 -0700

    solves problem 3

--- arrays_map_pracitce_problems.rb
+++ arrays_map_pracitce_problems.rb
@@ -22,6 +22,14 @@ p new_strings
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
+hashes = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+names = []
+
+hashes.each do |hash|
+  names.push(hash[:name])
+end
+p names
+
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
