3232ad0551c2b46ef727fa8ce874044a564e068d
RobynSpaulding <robynm27@gmail.com>
2023-04-13 11:30:32 -0700

    conditionals2 js problem 8, 9 10

--- conditionals_2.js
+++ conditionals_2.js
@@ -50,9 +50,9 @@ if (number < 10) {
 
 // 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
-var num1 = 1;
-var num2 = 2;
-if (num1 > 10 || num2 > 10) {
+var number1 = 1;
+var number2 = 2;
+if (number1 > 10 || number2 > 10) {
   console.log(100);
 } else {
   console.log(-100);
@@ -60,9 +60,45 @@ if (num1 > 10 || num2 > 10) {
 
 // 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
-var num = -12;
-if (num < 0) {
+var num4 = -12;
+if (num4 < 0) {
   console.log(1776);
 } else {
   console.log(1979);
+}
+
+// 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+var num5 = 100;
+if (num5 === 100) {
+  console.log(100);
+} else if (num5 === 99) {
+  console.log(99);
+} else {
+  console.log(0);
+}
+
+// 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+var num6 = 3;
+var num7 = 9;
+if (num6 < 0 && num7 > 0) {
+  console.log(1);
+} else {
+  console.log(0);
+}
+
+// 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+var num8 = 1;
+if (num8 > 80) {
+  console.log(5);
+} else if (num8 > 60) {
+  console.log(4);
+} else if (num8 > 40) {
+  console.log(3);
+} else if (num8 > 20) {
+  console.log(2);
+} else {
+  console.log(1);
 }
