1dc5a60b15417ad92425ab67899e4d8148ef010f
RobynSpaulding <robynm27@gmail.com>
2023-04-13 11:28:06 -0700

    conditionals2 js problem 2

--- conditionals_2.js
+++ conditionals_2.js
@@ -6,3 +6,13 @@ if (num === 10) {
 } else {
   console.log(-1);
 }
+// 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+var num2 = 10;
+if (num2 < 10) {
+  console.log(-1);
+} else if (num2 > 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
