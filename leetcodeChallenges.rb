4f3abb1d3b5511172a89df4d94c26d1c2f464c99
RobynSpaulding <robynm27@gmail.com>
2023-05-23 22:29:17 -0700

    adds and solves majority_element, includes both incorrect and solved

--- leetcodeChallenges.rb
+++ leetcodeChallenges.rb
@@ -36,3 +36,24 @@ def length_of_last_word(s)
   words = s.split(" ")
   p words[words.length-1].length
 end
+
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+# first attempt doesnt work
+def majority_element(nums)
+  max_num = nums[1]
+  nums.each do |num|
+      if num > (nums.length/2)
+          max_num = num
+      end
+  end 
+  p max_num
+end
+
+#correct 
+def majority_element(nums)
+  nums.sort[nums.length/2]
+end
