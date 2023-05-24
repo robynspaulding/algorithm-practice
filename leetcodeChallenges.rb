ec3d028111159766b21905ca837959e59d2b3008
RobynSpaulding <robynm27@gmail.com>
2023-05-23 22:31:26 -0700

    adds and solves Fizzbuzz in leetcode file

--- leetcodeChallenges.rb
+++ leetcodeChallenges.rb
@@ -57,3 +57,44 @@ end
 def majority_element(nums)
   nums.sort[nums.length/2]
 end
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+# first attempt doesnt work
+def fizz_buzz(n)
+    result = []
+    (1..n).each do |num|
+        if num % 3 == 0 && num % 5 == 0
+            result << "FizzBuzz"
+        elsif num % 3 == 3
+            result << "Fizz"
+        elsif num % 5 == 0
+            result << "Buzz"
+        else
+            result << num
+        end
+    end
+    p result
+end
+
+#correct
+def fizz_buzz(n)
+  result = []
+(1..n).each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    result << "FizzBuzz"
+  elsif num % 3 == 0
+    result << "Fizz"
+  elsif num % 5 == 0
+    result << "Buzz"
+  else 
+    result << num.to_s
+  end
+end
+p result
+end
