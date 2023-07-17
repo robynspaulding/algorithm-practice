3582c14323a6724e5a5e710fd1064704bb8706b3
RobynSpaulding <robynm27@gmail.com>
2023-07-16 20:16:16 -0700

    solves number 6 in new file

--- array_select_new.rb
+++ array_select_new.rb
@@ -55,4 +55,12 @@ p short_strings
 # should be ["a", "man", "a", "a"].
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+short_names = []
+items.each do |item|
+  if item[:name].length < 6
+    short_names << item
+  end
+end
+p short_names
+# should return [{name: "chair", price: 100}, {name: "book", price: 4}].
